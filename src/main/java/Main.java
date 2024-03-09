10c47230da1fb3cc144974683c8f9d93e3c07815
Dmitry Zolotov <zolotov_dmitry@mail.ru>
2024-03-09 21:06:51 +0300

    add Project

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+//        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+//        int sum = 0;
+//
+//        for (int i = 0; i < sales.length; i++) sum += sales[i];
+//        System.out.println(sum);
+    }
+}
